0caea917bf6be3838ba37e34e05803c64a3420a2
kossyak <akosyak@protonmail.com>
2020-07-22 14:38:11 +0400

    22.07

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const root = document.getElementById('form')
 const bNext = `<button class="next">Дальше</button>`
 const bBack = '<button class="back">Назад</button>'
-const bAnswer = `<button class="answer">Ответить</button>`
+const bAnswer = `<button class="apply">Ответить</button>`
+
+
+// import render from '/gfgjfgfh'
+//
+//
+//
+// export default function render() {
+//   const first = currentCardNum === 0
+//   root.innerHTML = createCard(cards[currentCardNum], first)
+// }
 
 let cards = cards_tests_new
 // let cards = cards_started
@@ -29,38 +39,61 @@ function createCard(card, first) {
 function createBody(card) {
     if (card.type_c === 'info') return card.body_c;
     else {
-        let answers = '<table>';
+        let answers = '<div class="answers">';
         for (let i=0; i < card.body_c.length; i++) {
             answers += createAnswer (card, i)
         }
-        answers += '</table>'
+        answers += '</div>'
         return answers
     }        
 }
+
+function createIconTrue() {
+  return `<div class="icon">
+            <object data="img/icons/right.svg" type="image/svg+xml"></object>
+          </div>`
+}
+function createIconFalse() {
+  return `<div class="icon">
+            <object data="img/icons/wrong.svg" type="image/svg+xml"></object>
+          </div>`
+}
+function checkIcon(card, i) {
+  if (card.user_answer) {
+    if (i === +card.r_ans) {
+      return createIconTrue()
+    }
+    if (+card.user_answer === i) {
+            if (card.user_answer === card.r_ans) {
+        return createIconTrue()
+      } else {
+        return createIconFalse()
+      }
+    }
+  }
+}
 function createAnswer (card, i) {
     return `
-    <tr>
-        <td class="icon">
-            <object data="img/icons/${createSVG(card, i)}.svg" type="image/svg+xml" data-svg="${i}" style="display: ${displaySVG(card, i)}"></object>
-        </td>
-        <td class="radio">    
+    <div class="answer">
+        ${checkIcon(card, i) || ''}
+        <label for="${i}" class="radio">    
             <input type="radio" name="r" id="${i}" ${isRight(card, i)} ${isChecked (card, i)}>      
-            <label for="${i}">${card.body_c[i]}</label><br>
-        </td>
-    </tr>
+            <span>${card.body_c[i]}</span>
+        </label>
+    </div>
    `
 }
 
 function createSVG (card, i) {
     return i == card.r_ans ? 'right' : 'wrong'
 }
-function displaySVG (card, i) {
-    if (!card.user_answer ) return 'none' // для новых вопросов
-    // для отмеченных вопросов:
-    if (i == card.r_ans) return 'block' // вывод правильного ответа
-    else if (card.user_answer == i) return 'block' // показать ответ юзера
-    else return 'none'
-}
+// function displaySVG (card, i) {
+//     if (!card.user_answer ) return 'none' // для новых вопросов
+//     // для отмеченных вопросов:
+//     if (i == card.r_ans) return 'block' // вывод правильного ответа
+//     else if (card.user_answer == i) return 'block' // показать ответ юзера
+//     else return 'none'
+// }
 function isRight (card, i) {
     return card.r_ans == i ? 'data-ans="1"' : ''
 }
@@ -88,9 +121,9 @@ root.onclick = (event) => {
     currentCardNum--
     render()
   }
-  if (event.target.className === 'answer') {
+  if (event.target.className === 'apply') {
     checkAnswer ()
-    document.querySelector('.answer').outerHTML = bNext
+    document.querySelector('.apply').outerHTML = bNext
 
     
   }
@@ -98,14 +131,16 @@ root.onclick = (event) => {
 
 function checkAnswer () { //Проверка правильного ответа
     let input = document.querySelector('input[data-ans="1"]');
+    const parent = document.querySelector('.answers')
     if (input.checked == true) { //если отмечен правильный импут
+        parent.childNodes[+input.id + 1].insertAdjacentHTML('afterbegin', createIconTrue())
         console.log('right');
-        document.querySelector(`object[data-svg="${input.id}"]`).style.display = 'block'
+        // document.querySelector(`object[data-svg="${input.id}"]`).style.display = 'block'
     // score++; // прибавить 1 балл
     } else {
         console.log('wrong');
     }
-    cards[currentCardNum].user_answer = input.id // сохранить ответ юзера в текущий массив
+    cards[currentCardNum].user_answer = input.id // сохранить ответ юзера в текущий массив // +
 }
 
 
